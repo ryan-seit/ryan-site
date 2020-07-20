a73a21584cdef10a7440b3e70b08f0572cb8ef55
Ryan Seit <ryan@ryanseit.com>
2020-07-20 14:54:03 -0400

    Organized assets, begun conversion of SVGs into components

--- src/containers/Pages/Technology/Technology.js
+++ src/containers/Pages/Technology/Technology.js
@@ -1,19 +1,52 @@
 import React from "react";
+import Slider from "react-slick";
 import classes from "./Technology.module.css";
 import TechnologyItem from "./TechnologyItem/TechnologyItem";
+
+const projects = {
+	p1: {
+		img:
+			"https://github.com/ryan-seit/drumblr-frontend/raw/master/drumblr-image.png",
+		title: "Drumblr",
+		links: {
+			github: "https://github.com/ryan-seit/ecommify-frontend",
+			medium:
+				"https://medium.com/@ryan_49675/beginners-guide-to-using-axios-23ca5922d183",
+			web:
+				"https://medium.com/@ryan_49675/beginners-guide-to-using-axios-23ca5922d183",
+		},
+		desc:
+			"Designed as an homage to late 80's/early 90's drum machines, Drumblr is a digital drum machine users can play in the browser, with customizable tempo, rhythms, and presets.",
+	},
+};
 
 // TODO: Add horizontal carousel layout
 // TODO: Add pagination
 
+const settings = {
+	centerMode: true,
+	infinite: true,
+	centerPadding: "60px",
+	// centerPadding: $(".slick").find(".single-slide-whatever-you-named-it").outerWidth() / 2,
+	slidesToShow: 1,
+	speed: 500,
+	arrows: false,
+};
+
 const Technology = props => {
 	return (
 		<div className={classes.Wrapper}>
-			<TechnologyItem />
-			{/* <TechnologyItem /> */}
-			{/* <TechnologyItem /> */}
-			<div className={classes.Pagination}>
-				<p>1 -- 2 -- 3 -- 4 -- 5</p>
-			</div>
+			<Slider {...settings}>
+				<div>
+					<TechnologyItem project={projects.p1} />
+				</div>
+				<div>
+					<TechnologyItem project={projects.p1} />
+				</div>
+				<div>
+					<TechnologyItem project={projects.p1} />
+				</div>
+			</Slider>
 		</div>
 	);
 };
--- src/containers/Pages/Technology/TechnologyItem/TechnologyItem.js
+++ src/containers/Pages/Technology/TechnologyItem/TechnologyItem.js
@@ -1,11 +1,20 @@
 import React from "react";
 
 import classes from "./TechnologyItem.module.css";
-import projectImage from "../../../../assets/project-image.svg";
+// import projectImage from "../../../../assets/project-image.svg";
 import Medium from "../../../../assets/medium-icon-circle.svg";
 import Github from "../../../../assets/github-icon-circle.svg";
 import Web from "../../../../assets/chrome-icon-circle.svg";
 
+import reactIcon from "../../../../assets/Icons/react.svg";
+import cssIcon from "../../../../assets/Icons/css.svg";
+import htmlIcon from "../../../../assets/Icons/html.svg";
+import jsIcon from "../../../../assets/Icons/js.svg";
+import pgIcon from "../../../../assets/Icons/pg.svg";
+import rubyIcon from "../../../../assets/Icons/ruby.svg";
+import railsIcon from "../../../../assets/Icons/rails.svg";
+
+import CSS from "../../../UI/Icons/CSS";
 // Renders an individual project, parent component Technology
 
 // TODO: (Stretch Goal) Abstract Image, Title, External Links and Description to state in order to dynamically generate them using an admin UI
@@ -13,43 +22,61 @@ import Web from "../../../../assets/chrome-icon-circle.svg";
 
 const TechnologyItem = props => (
 	<div className={classes.Technology}>
-		<img src={projectImage} alt='Placeholder' />
+		<div className={classes.Icons}>
+			<h3>Made with</h3>
+			<ul>
+				<li>
+					<img src={reactIcon} alt='React' />
+				</li>
+				<li>
+					<img src={jsIcon} alt='JavaScript' />
+				</li>
+				<li>
+					<img src={rubyIcon} alt='Ruby' />
+				</li>
+				<li>
+					<img src={railsIcon} alt='Rails' />
+				</li>
+				<li>
+					<img src={pgIcon} alt='PostgreSQL' />
+				</li>
+				<li>
+					{/* <img src={cssIcon} alt='CSS 3' /> */}
+					<CSS />
+				</li>
+				<li>
+					<img src={htmlIcon} alt='HTML 5' />
+				</li>
+			</ul>
+		</div>
+		<img
+			className={classes.Image}
+			src={props.project.img}
+			alt={props.project.title}
+		/>
 		<div className={classes.Details}>
 			{/* Project title */}
-			<h2>Project 1</h2>
+			<h2>{props.project.title}</h2>
 			{/* External links to relevant project information */}
 			<ul>
 				<li>
-					<a
-						href='https://medium.com/@ryan_49675/beginners-guide-to-using-axios-23ca5922d183'
-						alt='Medium Blog Post'
-					>
+					<a href={props.project.links.medium} alt='Medium Blog Post'>
 						<img src={Medium} alt='Medium' />
 					</a>
 				</li>
 				<li>
-					<a
-						href='https://github.com/ryan-seit/ecommify-frontend'
-						alt='Github Repo'
-					>
+					<a href={props.project.links.github} alt='Github Repo'>
 						<img src={Github} alt='Github' />
 					</a>
 				</li>
 				<li>
-					<a
-						href='https://medium.com/@ryan_49675/beginners-guide-to-using-axios-23ca5922d183'
-						alt='Website'
-					>
+					<a href={props.project.links.web} alt='Website'>
 						<img src={Web} alt='Website' />
 					</a>
 				</li>
 			</ul>
 			{/* Project summary */}
-			<p>
-				Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
-				fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem
-				sequi nesciunt.
-			</p>
+			<p>{props.project.desc}</p>
 		</div>
 	</div>
 );
