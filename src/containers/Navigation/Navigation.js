73e6629bd40c071bfa9a7fbbcb004dbd337ee7c3
Ryan Seit <ryan@ryanseit.com>
2020-07-21 16:42:48 -0400

    Completed navbar styling for desktop

--- src/containers/Navigation/Navigation.js
+++ src/containers/Navigation/Navigation.js
@@ -10,39 +10,66 @@ import GITHUBplain from "../UI/Icons/GITHUBplain";
 // Displays the Navigation Bar
 const navigation = props => {
 	return (
-		<div className={classes.Navigation}>
-			<NavLink to='/'>
-				<div className={classes.Logo}>
-					<LOGO />
-				</div>
-			</NavLink>
-			<NavLink to='/about'>
-				<div className={classes.About}>ABOUT</div>
-			</NavLink>
-			<NavLink to='/technology'>
-				<div className={classes.Technology}>TECHNOLOGY</div>
-			</NavLink>
-			<NavLink to='/music'>
-				<div className={classes.Music}>MUSIC</div>
-			</NavLink>
-			<NavLink to='/resume'>
-				<div className={classes.Resume}>RESUME</div>
-			</NavLink>
-			<NavLink to='/contact'>
-				<div className={classes.Contact}>CONTACT</div>
-			</NavLink>
+		<div className={classes.Wrapper}>
+			<div className={classes.Navigation}>
+				<NavLink to='/'>
+					<div className={classes.Logo}>
+						<LOGO />
+					</div>
+				</NavLink>
+				<NavLink
+					exact
+					to='/about'
+					className={classes.NavItem}
+					activeClassName={classes.selected}
+				>
+					ABOUT
+				</NavLink>
+				<NavLink
+					exact
+					to='/technology'
+					className={classes.NavItem}
+					activeClassName={classes.selected}
+				>
+					TECHNOLOGY
+				</NavLink>
+				<NavLink
+					exact
+					to='/music'
+					className={classes.NavItem}
+					activeClassName={classes.selected}
+				>
+					MUSIC
+				</NavLink>
+				<NavLink
+					exact
+					to='/resume'
+					className={classes.NavItem}
+					activeClassName={classes.selected}
+				>
+					RESUME
+				</NavLink>
+				<NavLink
+					exact
+					to='/contact'
+					className={classes.NavItem}
+					activeClassName={classes.selected}
+				>
+					CONTACT
+				</NavLink>
 
-			<div className={classes.External}>
-				<a href='https://www.linkedin.com/in/ryan-seit/'>
-					<div className={classes.LinkedIn}>
-						<LINKEDINplain />
-					</div>
-				</a>
-				<a href='https://github.com/ryan-seit'>
-					<div className={classes.Github}>
-						<GITHUBplain />
-					</div>
-				</a>
+				<div className={classes.External}>
+					<a href='https://www.linkedin.com/in/ryan-seit/'>
+						<div className={classes.LinkedIn}>
+							<LINKEDINplain />
+						</div>
+					</a>
+					<a href='https://github.com/ryan-seit'>
+						<div className={classes.Github}>
+							<GITHUBplain />
+						</div>
+					</a>
+				</div>
 			</div>
 		</div>
 	);
