c48849f9a81202e60cef41ec2b77dedcbdf1ed48
Ryan Seit <ryan@ryanseit.com>
2020-07-30 17:07:26 -0400

    Added ReactGA and History packages, configured Google Analytics for basic website traffic analysis

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Route, Switch, HashRouter } from "react-router-dom";
+import ReactGA from "react-ga";
+
 import Layout from "./containers/Layout/Layout";
 import Landing from "./containers/Pages/Landing/Landing";
 import Technology from "./containers/Pages/Technology/Technology";
@@ -7,21 +9,29 @@ import Music from "./containers/Pages/Music/Music";
 import About from "./containers/Pages/About/About";
 import Resume from "./containers/Pages/Resume/Resume";
 
+ReactGA.initialize("UA-174138511-1");
+
 const App = () => {
+	useEffect(() => {
+		ReactGA.pageview(window.location.pathname + window.location.search);
+	}, []);
+
 	return (
-		<div className='App'>
-			<Switch>
-				<Route path='/' exact component={Landing}>
-					<Landing />
-				</Route>
-				<Layout>
-					<Route path='/technology' component={Technology} />
-					<Route path='/music' component={Music} />
-					<Route path='/resume' component={Resume} />
-					<Route path='/about' component={About} />
-				</Layout>
-			</Switch>
-		</div>
+		<HashRouter basename='/'>
+			<div className='App'>
+				<Switch>
+					<Route path='/' exact component={Landing}>
+						<Landing />
+					</Route>
+					<Layout>
+						<Route path='/technology' component={Technology} />
+						<Route path='/music' component={Music} />
+						<Route path='/resume' component={Resume} />
+						<Route path='/about' component={About} />
+					</Layout>
+				</Switch>
+			</div>
+		</HashRouter>
 	);
 };
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { HashRouter } from "react-router-dom";
+// import { HashRouter } from "react-router-dom";
 
-const app = (
-	<HashRouter basename='/'>
-		<App />
-	</HashRouter>
-);
+const app = <App />;
 
 ReactDOM.render(app, document.getElementById("root"));
 
